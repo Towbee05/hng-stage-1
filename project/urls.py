140dff16135fe8bcd869456edf63d39480cb5242
Towbee05 <olatiseoluwatobiloba@gmail.com>
2026-05-06 23:45:59 +0100

    (fix)backend: completed backend logic

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.db_version, name='db_version'),
     path('api/profiles/', include('core.urls')),
     path('auth/', include('users.urls'))
 ]
